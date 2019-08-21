2b55e0909fbbc70bcc41b77e7fab9f3d6b071933
Adam Cardenas <cevaris@gmail.com>
2019-08-20 20:27:20 -0600

    update encoding

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ yarn add ts-node
 tsc --init
 npm i @types/node
 tsc -w
+
+
+```
+node bin/index.js write /tmp/test.txt "$(date)"
+node bin/index.js read /tmp/test.txt
+```
--- src/writeToFile.ts
+++ src/writeToFile.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 function writeToFile(filename: string, content: string): void {
-    fs.writeFile(filename, content, 'utf8', (err) => {
+    fs.writeFile(filename, content, {}, (err) => {
         if (err) console.error(err);
     });
 }
